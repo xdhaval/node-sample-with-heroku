0a0aaac1e1610b90d28231b596892a555319fb2e
dhaval.gohel <dhaval.gohel@exentia.com>
2021-06-09 18:41:26 +0530

    heroku rejectUnauthorized: true

--- app/models/index.js
+++ app/models/index.js
@@ -6,8 +6,9 @@ try {
     const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
         host: dbConfig.HOST,
         dialect: dbConfig.dialect,
-        native: false,
-        ssl: true,
+        ssl: {
+            rejectUnauthorized: true
+        },
         dialectOptions: {
             ssl: true
         },
